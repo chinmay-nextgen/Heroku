bce1eb2e56d9bc86edd093cb05b22bedc01ad57a
chinmay-nextgen <chinmaykota15@gmail.com>
2020-08-01 14:47:10 +0530

    changes

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 var port=process.env.PORT||5000
 var path =require('path')
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+}
 
 app.use(cors());
 const mongoose = require('mongoose');
